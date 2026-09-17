55c787c96951eb79225346f6cb5a5efe36dcf4b6
Daniil Ignatjev <daniil.ignatjev@gmail.com>
2026-09-17 18:32:51 +0300

    Added settings.json file

--- Autoclicker.cpp
+++ Autoclicker.cpp
@@ -1,5 +1,9 @@
 ﻿#include <iostream>
+#include <fstream>
 #include <windows.h>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 void click()
 {
@@ -14,23 +18,8 @@ void click()
 	SendInput(2, inputs, sizeof(INPUT));
 }
 
-int main()
+void autoclicker(char& startKey, char& stopKey, bool& canClick, double& clickInterval)
 {
-	char startKey{ 'B' };
-	char stopKey{ 'S' };
-	double clicksPerSecond = 1;
-
-	double clickInterval = 1000 / clicksPerSecond;
-	bool canClick = false;
-
-	std::cout << "Autoclicker launched successfully" << std::endl;
-	std::cout << "" << std::endl;
-	std::cout << "Autoclicker settings:" << std::endl;
-	std::cout << "Start Key: " << startKey << std::endl;
-	std::cout << "Stop Key: " << stopKey << std::endl;
-	std::cout << "Clicks Per Second: " << clicksPerSecond << std::endl;
-	std::cout << "" << std::endl;
-
 	while (true)
 	{
 		if (GetAsyncKeyState(startKey) & 0x8000 && !canClick)
@@ -60,6 +49,76 @@ int main()
 			Sleep(10);
 		}
 	}
+}
+
+void displayStartText(char &startKey, char &stopKey, double &clicksPerSecond)
+{
+	std::cout << "Autoclicker launched successfully" << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "Autoclicker settings:" << std::endl;
+	std::cout << "Start Key: " << startKey << std::endl;
+	std::cout << "Stop Key: " << stopKey << std::endl;
+	std::cout << "Clicks Per Second: " << clicksPerSecond << std::endl;
+	std::cout << "" << std::endl;
+}
+
+void setKeys(char &startKey, char &stopKey, double &clicksPerSecond, json &data)
+{
+	try
+	{
+		std::string startKeyString = data.value("start_key", "B");
+		startKey = startKeyString[0];
+
+		std::string stopKeyString = data.value("stop_key", "S");
+		stopKey = stopKeyString[0];
+
+		clicksPerSecond = data.value("clicks_per_second", 1);
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+}
+
+json readSettings()
+{
+	std::ifstream settings("settings.json");
+	json data;
+
+	if (settings.is_open())
+	{
+		data = json::parse(settings);
+	}
+	else
+	{
+		data = "error";
+	}
+
+	return data;
+}
+
+int main()
+{
+	json data = readSettings();
+
+	if (data == "error")
+	{
+		std::cout << "Error reading settings.json file" << std::endl;
+		return 0;
+	}
+
+	bool canClick = false;
+	char startKey;
+	char stopKey;
+	double clicksPerSecond;
+
+	setKeys(startKey, stopKey, clicksPerSecond, data);
+
+	double clickInterval = 1000 / clicksPerSecond;
+
+	displayStartText(startKey, stopKey, clicksPerSecond);
+
+	autoclicker(startKey, stopKey, canClick, clickInterval);
 
 	return 0;
 }
